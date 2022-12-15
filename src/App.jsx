33c1436d1f5dc29585ce22b830c0a17375107682
Berkai <berkay.e14005@gmail.com>
2022-12-15 16:31:25 +0300

    TaskList Updated.

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useState } from "react";
+
+// Custom Components
+import CustomForm from "./components/CustomForm";
+import TaskList from "./components/TaskList";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const AddTask = (task) => {
+    setTasks((prevState) => [...prevState, task]);
+  };
+
+  return (
+    <div className="container">
+      <header>
+        <h1>My Task List</h1>
+      </header>
+      <CustomForm addTask={AddTask} />
+      {tasks && <TaskList tasks={tasks} />}
+    </div>
+  );
+}
+
+export default App;
