3532c7f1edd485e73de6318ecd938824f786814d
Berkai <berkay.e14005@gmail.com>
2022-12-15 19:45:03 +0300

    commitlendi

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,29 @@ function App() {
     setTasks((prevState) => [...prevState, task]);
   };
 
+  const deleteTask = (id) => {
+    setTasks((prevState) => prevState.filter((t) => t.id !== id));
+  };
+
+  const toggleTask = (id) => {
+    setTasks((prevState) =>
+      prevState.map((t) => (t.id === id ? { ...t, checked: !t.checked } : t))
+    );
+  };
+
   return (
     <div className="container">
       <header>
         <h1>My Task List</h1>
       </header>
       <CustomForm addTask={AddTask} />
-      {tasks && <TaskList tasks={tasks} />}
+      {tasks && (
+        <TaskList
+          tasks={tasks}
+          deleteTask={deleteTask}
+          toggleTask={toggleTask}
+        />
+      )}
     </div>
   );
 }
